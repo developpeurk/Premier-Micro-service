60edbb513cbb230202d86740565877d6e6e42bc5
developpeurk <developpeurk@gmail.com>
2022-10-23 21:01:10 +0100

    RepositoryRestResource

--- src/main/java/com/lambarki/yassine/bankserviceyassinelambarki/repositories/BankAccountRepository.java
+++ src/main/java/com/lambarki/yassine/bankserviceyassinelambarki/repositories/BankAccountRepository.java
@@ -1,7 +1,14 @@
 package com.lambarki.yassine.bankserviceyassinelambarki.repositories;
 
 import com.lambarki.yassine.bankserviceyassinelambarki.entities.BankAccount;
+import com.lambarki.yassine.bankserviceyassinelambarki.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
+
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
+    List<BankAccount> findByType(AccountType type);
 }
--- src/main/java/com/lambarki/yassine/bankserviceyassinelambarki/web/AccountRestController.java
+++ src/main/java/com/lambarki/yassine/bankserviceyassinelambarki/web/AccountRestController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api")
 @AllArgsConstructor
 public class AccountRestController {
 
