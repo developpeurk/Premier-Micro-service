b9b91880525ed376b32ec6a020a6fe2afc155a1a
developpeurk <developpeurk@gmail.com>
2022-10-23 21:14:58 +0100

    restresource

--- src/main/java/com/lambarki/yassine/bankserviceyassinelambarki/repositories/BankAccountRepository.java
+++ src/main/java/com/lambarki/yassine/bankserviceyassinelambarki/repositories/BankAccountRepository.java
@@ -3,12 +3,15 @@ package com.lambarki.yassine.bankserviceyassinelambarki.repositories;
 import com.lambarki.yassine.bankserviceyassinelambarki.entities.BankAccount;
 import com.lambarki.yassine.bankserviceyassinelambarki.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
 
 @RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
-    List<BankAccount> findByType(AccountType type);
+    @RestResource(path = "/byType")
+    List<BankAccount> findByType(@Param("t") AccountType type);
 }
