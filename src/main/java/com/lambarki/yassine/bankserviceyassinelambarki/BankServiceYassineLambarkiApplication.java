200a3a5365d8f5f9b4d56f200f37b13e47942346
developpeurk <developpeurk@gmail.com>
2022-10-23 18:41:43 +0100

    first commit

---
+++ src/main/java/com/lambarki/yassine/bankserviceyassinelambarki/BankServiceYassineLambarkiApplication.java
@@ -0,0 +1,13 @@
+package com.lambarki.yassine.bankserviceyassinelambarki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServiceYassineLambarkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankServiceYassineLambarkiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lambarki/yassine/bankserviceyassinelambarki/BankServiceYassineLambarkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambarki.yassine.bankserviceyassinelambarki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceYassineLambarkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
